aae407e0f0509fe17575660a2819948c7f8a133e
YeeunHeo <gjdpdms2005@gmail.com>
2020-12-13 03:10:48 +0900

    Fix Header height in AdminTable component

--- app/components/atoms/AdminTable.jsx
+++ app/components/atoms/AdminTable.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Grid,
   Button,
@@ -11,6 +11,8 @@ import {
   Checkbox,
   Divider,
   TableFooter,
+  TablePagination,
+  Link,
 } from '@material-ui/core';
 import Pagination from '@material-ui/lab/Pagination';
 import { makeStyles, useTheme } from '@material-ui/styles';
@@ -31,8 +33,88 @@ const rows = [
     'lee@naver.com',
     'o'
   ),
-  createData(4, '소프트웨어학부', 20171234, '조예은', 'cho@naver.com', 'o'),
-  createData(5, '소프트웨어학부', 20171234, '정예은', 'jeong@naver.com', 'o'),
+  createData(4, '소프트웨어학부', 20180000, '조예은', 'cho@naver.com', 'o'),
+  createData(5, '소프트웨어학부', 20200000, '허혜은', 'jeong@naver.com', 'o'),
+  createData(
+    6,
+    '소프트웨어학부',
+    20190000,
+    '김혜은',
+    '20190000@naver.com',
+    'x'
+  ),
+  createData(
+    7,
+    '소프트웨어학부',
+    20170000,
+    '정혜은',
+    '20170000@naver.com',
+    'x'
+  ),
+  createData(
+    8,
+    '소프트웨어학부',
+    20180001,
+    '정은혜',
+    '20180001@naver.com',
+    'o'
+  ),
+  createData(
+    9,
+    '소프트웨어학부',
+    20180002,
+    '조혜은',
+    '20180002@naver.com',
+    'x'
+  ),
+  createData(
+    10,
+    '소프트웨어학부',
+    20180003,
+    '조은혜',
+    '20180003@naver.com',
+    'o'
+  ),
+  createData(
+    11,
+    '소프트웨어학부',
+    20180004,
+    '허은혜',
+    '20180004@naver.com',
+    'o'
+  ),
+  createData(
+    12,
+    '소프트웨어학부',
+    20180005,
+    '정승욱',
+    '20180005@naver.com',
+    'x'
+  ),
+  createData(
+    13,
+    '소프트웨어학부',
+    20180006,
+    '김승욱',
+    '20180006@naver.com',
+    'o'
+  ),
+  createData(
+    14,
+    '소프트웨어학부',
+    20180007,
+    '조승욱',
+    '20180007@naver.com',
+    'x'
+  ),
+  createData(
+    15,
+    '소프트웨어학부',
+    201800008,
+    '허승욱',
+    '201800008@naver.com',
+    'o'
+  ),
 ];
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -45,6 +127,7 @@ const useStyles = makeStyles((theme) => ({
   },
   tableCell: {
     fontSize: 18,
+    fontWeight: 400,
   },
   tableCellRow: {
     fontSize: 18,
@@ -71,12 +154,18 @@ const useStyles = makeStyles((theme) => ({
 
 export default function AdminTable() {
   const classes = useStyles();
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
   return (
     <>
       <Card className={classes.root}>
         <Table className={classes.table} size="small">
           <TableHead>
-            <TableRow>
+            <TableRow style={{ height: 50.5 }}>
               <TableCell
                 className={classes.tableCell}
                 style={{ paddingLeft: 25 }}
@@ -96,50 +185,63 @@ export default function AdminTable() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
-              <TableRow key={row.id}>
-                <TableCell>
-                  <Checkbox color="primary" />
-                </TableCell>
-                <TableCell className={classes.tableCellRow}>
-                  {row.major}
-                </TableCell>
-                <TableCell className={classes.tableCellRow}>
-                  {row.studentId}
-                </TableCell>
-                <TableCell className={classes.tableCellRow}>
-                  {row.name}
-                </TableCell>
-                <TableCell className={classes.tableCellRow}>
-                  {row.email}
-                </TableCell>
-                <TableCell className={classes.tableCellRow}>
-                  {row.isAllowed === 'x' ? (
-                    <>
-                      <Button color="primary" style={{ fontSize: 18 }}>
-                        승인
-                      </Button>
-                      /
-                      <Button color="primary" style={{ fontSize: 18 }}>
-                        거절
-                      </Button>
-                    </>
-                  ) : (
-                    <div>
-                      <Button disabled>-</Button>
-                    </div>
-                  )}
-                </TableCell>
-              </TableRow>
-            ))}
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => (
+                <TableRow key={row.id}>
+                  <TableCell>
+                    <Checkbox color="primary" />
+                  </TableCell>
+                  <TableCell className={classes.tableCellRow}>
+                    {row.major}
+                  </TableCell>
+                  <TableCell className={classes.tableCellRow}>
+                    {row.studentId}
+                  </TableCell>
+                  <TableCell className={classes.tableCellRow}>
+                    {row.name}
+                  </TableCell>
+                  <TableCell className={classes.tableCellRow}>
+                    {row.email}
+                  </TableCell>
+                  <TableCell className={classes.tableCellRow}>
+                    {row.isAllowed === 'x' ? (
+                      <>
+                        <Link
+                          href="/#"
+                          color="primary"
+                          style={{ fontSize: 18 }}
+                        >
+                          승인
+                        </Link>
+                        &nbsp;/&nbsp;
+                        <Link
+                          href="/#"
+                          color="primary"
+                          style={{ fontSize: 18 }}
+                        >
+                          거절
+                        </Link>
+                      </>
+                    ) : (
+                      <div>
+                        <Button disabled>-</Button>
+                      </div>
+                    )}
+                  </TableCell>
+                </TableRow>
+              ))}
           </TableBody>
           <caption className={classes.caption}>
             <Divider className={classes.divider} />
             <Pagination
               className={classes.pagination}
-              count={10}
+              count={parseInt(rows.length / 10 + 1, 10)}
+              onClick={handleChangePage}
+              defaultPage={1}
               shape="rounded"
               color="primary"
+              rowsPerPage={rowsPerPage}
               hidePrevButton
               hideNextButton
             />
