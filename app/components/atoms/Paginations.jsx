e97d33ca9492d3ce4889b91f70db0193365be000
YeeunHeo <gjdpdms2005@gmail.com>
2020-12-13 23:09:36 +0900

    Implement Pagination in Supervise component

--- app/components/atoms/Paginations.jsx
+++ app/components/atoms/Paginations.jsx
@@ -37,7 +37,8 @@ const Paginations = (props) => {
                     '0px 1px 0px rgba(63, 63, 68, 0.14), 0px 3px 4px rgba(63, 63, 68, 0.12), 0px 1px 5px rgba(63, 63, 68, 0.2)',
                   minWidth: 32,
                   maxHeight: 32,
-                  margin: 8,
+                  marginLeft: 8,
+                  marginRight: 8,
                 }}
               >
                 <p style={{ marginTop: -2 }}>{page}</p>
@@ -54,7 +55,8 @@ const Paginations = (props) => {
                     '0px 1px 0px rgba(63, 63, 68, 0.14), 0px 3px 4px rgba(63, 63, 68, 0.12), 0px 1px 5px rgba(63, 63, 68, 0.2)',
                   minWidth: 32,
                   maxHeight: 32,
-                  margin: 8,
+                  marginLeft: 8,
+                  marginRight: 8,
                   textAlign: 'justify',
                 }}
               >
